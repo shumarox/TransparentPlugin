020e6aa262eb75f56ea7be0dd6f6b28272af2127
shumaro <shumarox@gmail.com>
2020-10-17 13:39:43 +0900

    メニューバーのホイールスクロールで透明度を変更

--- src/TransparentAction.java
+++ src/TransparentAction.java
@@ -1,5 +1,6 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,21 +9,37 @@ import java.lang.reflect.Field;
 
 public class TransparentAction extends AnAction {
 
-    private boolean transparent = false;
+    private float opacity = 1.0f;
+
+    private boolean mouseWheelListenerAdded = false;
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent e) {
         try {
             JFrame frame = (JFrame) ((JWindow) e.getInputEvent().getSource()).getParent();
             Field field = Component.class.getDeclaredField("peer");
             field.setAccessible(true);
             WindowPeer peer = (WindowPeer) field.get(frame);
 
-            transparent = !transparent;
-            float opacity = transparent ? 0.8f : 1f;
+            opacity = opacity == 1.0f ? 0.8f : 1.0f;
             peer.setOpacity(opacity);
+
+            if (!mouseWheelListenerAdded) {
+                addMouseWheelListener(frame, peer);
+            }
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private void addMouseWheelListener(JFrame frame, WindowPeer peer) {
+        JMenuBar menuBar = frame.getJMenuBar();
+
+        menuBar.addMouseWheelListener(mouseWheelEvent -> {
+            opacity = Math.max(Math.min(opacity - mouseWheelEvent.getWheelRotation() / 20f, 1.0f), 0.3f);
+            peer.setOpacity(opacity);
+        });
+
+        mouseWheelListenerAdded = true;
     }
 }
